fefea3e113fba10920c0219afbc8cdf1481d7628
Arfi Hussain <arfihussain27@gmail.com>
2025-04-25 23:47:45 +0530

    updated test

--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -1,18 +1,9 @@
 import React from "react";
-import SplitPane, {Pane} from "react-split-pane";
-import "./splitPaneStyles.css";
 
 function Test() {
   return (
     <div>
-      <SplitPane style={{position: "static", marginBottom: "-2.96rem"}} split="vertical" defaultSize="50%">
-        <div className="bg-red-200 h-[120px]">
-            Left div
-        </div>
-        <div className="bg-red-600 h-[120px]">
-            Right div
-        </div>
-      </SplitPane>
+      <p>test</p>
     </div>
   );
 }
